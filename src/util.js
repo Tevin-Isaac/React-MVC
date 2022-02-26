9169a78dd1aa6ee0cb1ecb25b469666d24169504
Tevin-creator <isaactevin67@gmail..com>
2022-02-26 03:12:35 +0300

    sorted variable error in util.js file

--- src/util.js
+++ src/util.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     formatCurrency: function (num) {
         return '$' + Number(num.toFixed(1)).toLocaleString() + ' ';
